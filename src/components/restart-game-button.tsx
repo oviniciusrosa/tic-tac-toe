16fe8f53e2b47d6e4f847bc5050ae1f9ff36ec6b
oviniciusrosa <vinicius.rosa@stocci.com>
2025-05-15 10:26:10 -0300

    fix: reset on player vs player

--- src/components/restart-game-button.tsx
+++ src/components/restart-game-button.tsx
@@ -8,12 +8,18 @@ import { useGameStore } from "@/store/game.store";
 export function RestartGameButton() {
   const game = useGameStore();
 
+  function resetGame() {
+    if (!game.gameMode) return;
+
+    game.restart(game.gameMode, game.mainPlayer);
+  }
+
   return (
     <Animation.GrowEffect>
       <button
         type="button"
         className="flex items-center justify-center bg-neutral-silver-dark rounded-xl w-14 h-14 border-b-6 border-neutral-silver-border cursor-pointer"
-        onClick={() => game.restart()}
+        onClick={resetGame}
       >
         <Image
           src="/icons/icon-restart.svg"
